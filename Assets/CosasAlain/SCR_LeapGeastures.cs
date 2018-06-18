f92a1f29847710cfc80945e865420d17676b358b
Alain <barraganalain55@gmail.com>
2018-06-17 21:54:02 -0500

    Uff que no hice

---
+++ Assets/CosasAlain/SCR_LeapGeastures.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SCR_LeapGeastures : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    public void Shot()
+    {
+        anim.SetTrigger("Fall");
+    }
+
+    public void Wave()
+    {
+        anim.SetTrigger("Wave");
+    }
+
+    public void no()
+    {
+        anim.SetTrigger("Shake");
+    }
+}
